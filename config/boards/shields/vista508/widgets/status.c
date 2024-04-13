0f82f6ada09ff01ab428c76b40cccea9c97f5fdc
MickiusMousius <MickiusMousius@icloud.com>
2024-04-13 21:33:54 +0930

    Make everything work with the latest Zephyr updates

--- config/boards/shields/vista508/widgets/status.c
+++ config/boards/shields/vista508/widgets/status.c
@@ -176,7 +176,7 @@ static void draw_bottom(lv_obj_t *widget, lv_color_t cbuf[], const struct status
 
     // Draw layer
     if (state->layer_label == NULL) {
-        char text[9] = {};
+        char text[10] = {};
 
         sprintf(text, "LAYER %i", state->layer_index);
 
@@ -203,7 +203,7 @@ static void battery_status_update_cb(struct battery_status_state state) {
 }
 
 static struct battery_status_state battery_status_get_state(const zmk_event_t *eh) {
-    return (struct battery_status_state) {
+    return (struct battery_status_state){
         .level = bt_bas_get_battery_level(),
 #if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
         .usb_present = zmk_usb_is_powered(),
